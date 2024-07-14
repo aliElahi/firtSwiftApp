101906d375b0298e9748668f9896a6b2da31e15e
ali <ali@hamrahkish.com>
2024-07-14 14:03:01 +0330

    initial commit

---
+++ FirstApp/FirstApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by Ali Elahi on 7/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Ali Elahi on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
